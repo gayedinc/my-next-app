6c4f033a2a1c99a7050ecceeea0ac5f295def882
[Gaye Dinç] <gayedinc190@gmail.com>
2025-05-08 17:57:30 +0300

    link componentler oluşturuldu ve buradan detay sayfalarına gidildi

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
     <>
-      <h2>
-        Blog Sayfam
-      </h2>
+      <h3>Son Yazılarım</h3>
+      <ul>
+        <li><Link href={'/blog/merhaba-dunya'}>Merhaba Dünya</Link></li>
+        <li><Link href={'/blog/ilk-next-projesi'}>Next Projesi</Link></li>
+      </ul>
     </>
   )
 }
